890cd1f29da5e4e5c322910255a90a5a2acda401
hezg <hzgdeemail@163.com>
2022-09-12 23:52:47 +0800

    feat: promise 链式调用

--- promise.js
+++ promise.js
@@ -1,6 +1,6 @@
 // promise
 
-// 先定义三个常量表示状态
+// 先定义三个常量表示状态，状态只能由 Pending --> Fulfilled 或者 Pending --> Rejected，且一但发生改变便不可二次修改；
 const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
@@ -24,14 +24,29 @@ class MyPromise {
   // 失败之后的原因
   reason = null
 
+  // 存储成功回调函数
+  // onFulfilledCallback = null
+  onFulfilledCallbacks = []
+  // 存储失败回调函数
+  // onRejectedCallback = null
+  onRejectedCallbacks = []
+
   // 更改成功后的状态
   resolve = (value) => {
+    console.log('value,', value)
     // 只有状态是等待，才执行状态修改
     if (this.status == PENDING) {
       // 修改成功后的值。
       this.status = FULFILLED
       // 保存成功之后的值
       this.value = value
+
+      // 判断成功回调是否存在，如果存在就调用
+      // resolve里面将所有成功的回调拿出来执行
+      while (this.onFulfilledCallbacks.length) {
+        // Array.shift() 取出数组第一个元素，然后（）调用，shift不是纯函数，取出后，数组将失去该元素，直到数组为空
+        this.onFulfilledCallbacks.shift()(value)
+      }
     }
   }
   // 更改失败后的状态
@@ -42,18 +57,47 @@ class MyPromise {
       this.status = REJECTED
       // 保存失败后的原因
       this.reason = reason
+
+      // reject里面将所有失败的回调拿出来执行
+      while (this.onRejectedCallbacks.length) {
+        this.onRejectedCallbacks.shift()(reason)
+      }
     }
   }
 
   then(onFulfilled, onRejected) {
-    // 判断状态
-    if (this.status === FULFILLED) {
-      // 调用成功回调，并且把值返回
-      onFulfilled(this.value)
-    } else if (this.status === REJECTED) {
-      // 调用失败回调，并且把原因返回
-      onRejected(this.reason)
-    }
+    //return一个新的promise
+    return new MyPromise((resolve, reject) => {
+      // 判断状态
+      if (this.status === FULFILLED) {
+        // 调用成功回调，并且把值返回
+        // 获取成功回调函数的执行结果
+        const x = onFulfilled(this.value)
+        // 传入 resolvePromise 集中处理
+        resolvePromise(x, resolve, reject)
+      } else if (this.status === REJECTED) {
+        // 调用失败回调，并且把原因返回
+        onRejected(this.reason)
+      } else if (this.status === PENDING) {
+        // 因为不知道后面状态的变化情况，所以将成功回调和失败回调存储起来。
+        // 等到执行成功失败函数的时候再传递
+        this.onFulfilledCallbacks.push(onFulfilled)
+        this.onRejectedCallbacks.push(onRejected)
+      }
+    })
+  }
+}
+function resolvePromise(x, resolve, reject) {
+  // 判断x是不是 MyPromise 实例对象,
+  // /分类讨论返回值,如果是Promise,那么等待Promise状态变更,否则直接resolve
+  if (x instanceof MyPromise) {
+    // 执行 x，调用 then 方法，目的是将其状态变为 fulfilled 或者 rejected
+    // x.then(value => resolve(value), reason => reject(reason))
+    // 简化之后
+    x.then(resolve, reject)
+  } else {
+    // 普通值
+    resolve(x)
   }
 }
 
--- test.js
+++ test.js
@@ -3,14 +3,45 @@
 // 引入我们的 MyPromise.js
 const MyPromise = require('./promise')
 const promise = new MyPromise((resolve, reject) => {
-   resolve('success')
-   reject('err')
+  // settimeout
+  setTimeout(() => {
+    resolve('success')
+  }, 1000)
 })
 
+// promise.then(
+//   (value) => {
+//     console.log(1);
+//     console.log('resolve', value)
+//   },
+//   (reason) => {
+//     console.log('reject', reason)
+//   }
+// )
+// promise.then(value => {
+//   console.log(2)
+//   console.log('resolve', value)
+// })
+
+// promise.then(value => {
+//   console.log(3)
+//   console.log('resolve', value)
+// })
+
+// 链式调用
+function other () {
+  return new MyPromise((resolve, reject) =>{
+    resolve('other')
+  })
+}
+
 promise.then(value => {
+  console.log(1)
   console.log('resolve', value)
-}, reason => {
-  console.log('reject', reason)
+  return other()
+}).then(value => {
+  console.log(2)
+  console.log('resolve', value)
 })
 
 // 执行结果：resolve success
